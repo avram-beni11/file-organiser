fa91b5bbdfb0246e60b725706443dd393d62ee0f
avram-beni11 <116164939+avram-beni11@users.noreply.github.com>
2024-03-03 13:00:05 +0000

    added emu function
    added function for emu, just does what it did before, but now calls a function instead

--- File Organiser.py
+++ File Organiser.py
@@ -21,41 +21,69 @@ import zipfile
 #Change dir to "Downloads"
 os.chdir("C:\\Users\\beni_\\Downloads")
 
+def dolphin_emu():
+    with zipfile.ZipFile(file, 'r') as zip_file:
+            file_list = zip_file.namelist()
+            print("The file list is:", file_list)
+            for rom_name in file_list:
+                file_name, ext = os.path.splitext(rom_name)
+                #if rom_name.endswith(".gba"):
+                os.chdir("D:\\#Python File Organiser")
+                #Remove extension period, capitalises it, and makes a new folder with the capitalised name
+                remove_stop = ext.replace(".", "")
+                folder_name = remove_stop.upper()
+                Path(folder_name).mkdir(exist_ok=True)
+                #Create variable "new_dir", which concatinates our desired final directory with the folder name created earluer
+                new_dir = ("D:\\#Python File Organiser\\" + folder_name)
+                os.chdir("C:\\Users\\beni_\\Downloads")
+                print(file)
+                print("Function Called")
+                try:
+                    shutil.move(file, new_dir)
+                    #os.remove(file + ".zip")
+                except WindowsError:
+                    pass
 
 #Loops through directory
 for file in os.listdir():
     file_name, ext = os.path.splitext(file)
 
     #Checks for specific extension
-    if ext == ".png":
-        os.chdir("D:\\#Python File Organiser")
-        #Remove extension period, capitalises it, and makes a new folder with the capitalised name
-        remove_stop = ext.replace(".", "")
-        folder_name = remove_stop.upper()
-        Path(folder_name).mkdir(exist_ok=True)
-        #Create variable "new_dir", which concatinates our desired final directory with the folder name created earluer
-        new_dir = ("D:\\#Python File Organiser\\" + folder_name) 
-        #Change dir back to "Downloads"
-        os.chdir("C:\\Users\\beni_\\Downloads")
-        print("File: " + file + " has been moved to: " + new_dir)
-        #Move files from "Downloads" to our new folder named after the files extension
-        shutil.move(file, new_dir)
+    # if ext == ".png":
+    #     os.chdir("D:\\#Python File Organiser")
+    #     #Remove extension period, capitalises it, and makes a new folder with the capitalised name
+    #     remove_stop = ext.replace(".", "")
+    #     folder_name = remove_stop.upper()
+    #     Path(folder_name).mkdir(exist_ok=True)
+    #     #Create variable "new_dir", which concatinates our desired final directory with the folder name created earluer
+    #     new_dir = ("D:\\#Python File Organiser\\" + folder_name) 
+    #     #Change dir back to "Downloads"
+    #     os.chdir("C:\\Users\\beni_\\Downloads")
+    #     print("File: " + file + " has been moved to: " + new_dir)
+    #     #Move files from "Downloads" to our new folder named after the files extension
+    #     shutil.move(file, new_dir)
 
+    # if ext == ".zip":
+    #     with zipfile.ZipFile(file, 'r') as zip_file:
+    #         file_list = zip_file.namelist()
+    #         print("The file list is:", file_list)
+    #         for rom_name in file_list:
+    #             file_name, ext = os.path.splitext(rom_name)
+    #             #if rom_name.endswith(".gba"):
+    #             os.chdir("D:\\#Python File Organiser")
+    #             #Remove extension period, capitalises it, and makes a new folder with the capitalised name
+    #             remove_stop = ext.replace(".", "")
+    #             folder_name = remove_stop.upper()
+    #             Path(folder_name).mkdir(exist_ok=True)
+    #             #Create variable "new_dir", which concatinates our desired final directory with the folder name created earluer
+    #             new_dir = ("D:\\#Python File Organiser\\" + folder_name)
+    #             os.chdir("C:\\Users\\beni_\\Downloads")
+    #             #print(file)
+    #             try:
+    #                 shutil.move(file, new_dir)
+    #                 #os.remove(file + ".zip")
+    #             except WindowsError:
+    #                 pass
+        
     if ext == ".zip":
-        with zipfile.ZipFile(file, 'r') as zip_file:
-            file_list = zip_file.namelist()
-            print("The file list is:", file_list)
-            for rom_name in file_list:
-                file_name, ext = os.path.splitext(rom_name)
-                if rom_name.endswith(".gba"):
-                    os.chdir("D:\\#Python File Organiser")
-                    #Remove extension period, capitalises it, and makes a new folder with the capitalised name
-                    remove_stop = ext.replace(".", "")
-                    folder_name = remove_stop.upper()
-                    Path(folder_name).mkdir(exist_ok=True)
-                    #Create variable "new_dir", which concatinates our desired final directory with the folder name created earluer
-                    new_dir = ("D:\\#Python File Organiser\\" + folder_name)
-                    os.chdir("C:\\Users\\beni_\\Downloads")
-                    shutil.move(file, new_dir)
-                    
-                    #print(f"Found .gba file: {file_name}")
+        dolphin_emu()
