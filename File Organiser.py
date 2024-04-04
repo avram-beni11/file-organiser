36aabec3ddc5f35cee51ff0194b9ce4d7626bf32
avram-beni11 <116164939+avram-beni11@users.noreply.github.com>
2024-04-04 15:28:26 +0100

    start and end dir kinda work

--- File Organiser.py
+++ File Organiser.py
@@ -65,22 +65,32 @@ root.configure(bg="#5ea6ee")
 for file in os.listdir():
     file_name, ext = os.path.splitext(file)
 
-    def fileMove(displayContent):
+    def fileMove():
         #Checks for specific extension
-        if ext == ".png":
+        # if ext == ".webp":
             #os.chdir("D:\\#Python File Organiser")
-            os.chdir(displayContent)
+            global startDir
+            global endDir
+            #print(startDir.get())
+            # fixed_start = startDir.get().replace("\\", "\\\\")
+            # fixed_end = endDir.get().replace("\\", "\\\\")
+            os.chdir(startDir.get())
             #Remove extension period, capitalises it, and makes a new folder with the capitalised name
             remove_stop = ext.replace(".", "")
             folder_name = remove_stop.upper()
             Path(folder_name).mkdir(exist_ok=True)
             #Create variable "new_dir", which concatinates our desired final directory with the folder name created earluer
-            new_dir = ("D:\\#Python File Organiser\\" + folder_name) 
+            # new_dir = ("D:\\#Python File Organiser\\" + folder_name) 
+            new_dir = (endDir.get() + "\\" + folder_name) 
             #Change dir back to "Downloads"
-            os.chdir("C:\\Users\\beni_\\Downloads")
-            print("File: " + file + " has been moved to: " + new_dir)
+            # os.chdir("C:\\Users\\beni_\\Downloads")
+            os.chdir(startDir.get())
+            print("File: " + file + " has been moved to: " + new_dir + " from " + startDir.get())
             #Move files from "Downloads" to our new folder named after the files extension
             shutil.move(file, new_dir)
+            
+    # def printTest():
+    #     print("Hello World")
 
     # if ext == ".zip":
     #     with zipfile.ZipFile(file, 'r') as zip_file:
@@ -126,23 +136,23 @@ for file in os.listdir():
 displayContent = " "
 textIn = StringVar()
 
-def temp_text_start(e):
-   startDir.delete(0,"end")
+# def temp_text_start(e):
+#    startDir.delete(0,"end")
 
-def temp_text_end(e):
-   endDir.delete(0,"end")
+# def temp_text_end(e):
+#    endDir.delete(0,"end")
 
 startDir = Entry(root, textvariable=textIn, width=40)
 startDir.grid(row=0, column=0, columnspan=4, padx=10, pady=10)
 startDir.insert(0, "Enter starting directory...")
-startDir.bind("<FocusIn>", temp_text_start)
+# startDir.bind("<FocusIn>", temp_text_start)
 
 endDir = Entry(root, width=40)
 endDir.grid(row=1, column=0, columnspan=4, padx=10, pady=10)
 endDir.insert(0, "Enter end directory...")
-endDir.bind("<FocusIn>", temp_text_end)
+# endDir.bind("<FocusIn>", temp_text_end)
 
-moveBtn = Button(root, text="Move Files", padx=20, pady=1, command=fileMove(displayContent))
+moveBtn = Button(root, text="Move Files", padx=20, pady=1, command=fileMove)
 moveBtn.grid(row=2)
 
 root.mainloop()
